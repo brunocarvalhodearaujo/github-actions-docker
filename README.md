3e38022e9c3ffd2d8db8ceea4745cb9642f41ab8
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2025-03-11 22:33:44 +0000

    Add Dockerfile, docker-compose, and startup script for GitHub Actions runner

---
+++ README.md
@@ -0,0 +1,3 @@
+## GitHub Actions Runner
+
+- [How to containerize a GitHub Actions self-hosted runner](https://baccini-al.medium.com/how-to-containerize-a-github-actions-self-hosted-runner-5994cc08b9fb)
---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/docker/actions-runner
+
+./config.sh --token ${TOKEN}
+
+cleanup() {
+  echo "Removing runner..."
+  ./config.sh remove --unattended --token ${TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
